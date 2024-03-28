f60e8e3fad496106ea41c716e6e5e680e6cf40c3
dlaurianti <dlaurianti@uiowa.edu>
2024-03-28 00:35:05 -0500

    feat: print uri to serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,67 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include "wifi.h"
+
+int status = WL_IDLE_STATUS;
+int led = LED_BUILTIN;
+
+WiFiServer server(80);
 
 void setup() {
+    pinMode(led, OUTPUT);
+    digitalWrite(led, LOW);
+    Serial.begin(9600);
+    // Check the wifi hardware is working
+    if (WiFi.status() == WL_NO_MODULE) {
+        Serial.println("WiFi Module Failed!");
+        exit(1);
+    }
+    // Connect to the wifi network
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to WPA SSID: ");
+        Serial.println(ssid);
+        status = WiFi.begin(ssid, pass);
+    }
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+    Serial.print("Access the server at http://");
+    Serial.println(ip);
+
+    server.begin();
+    digitalWrite(led, HIGH);
 }
 
 void loop() {
+    WiFiClient client = server.available();
+    if (client) {
+        Serial.println("Client Connected...");
+        bool currentLineBlank = true;
+        while (client.connected()) {
+            if (client.available()) {
+                char c = client.read();
+                Serial.write(c);
+                if (c == '\n' && currentLineBlank) {
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-type:text/html");
+                    client.println();
+                    client.println("<!DOCTYPE HTML>");
+                    client.println("<html>");
+                    client.println("Message from Nano");
+                    client.println("</html>");
+                    client.println();
+                    break;
+                }
+                if (c == '\n') {
+                    currentLineBlank = true;
+                } else if (c != '\r') {
+                    currentLineBlank = false;
+                }
+            }
+        }
+        delay(100);
+        client.stop();
+        Serial.println("Client Disconnected.");
+    }
 }
